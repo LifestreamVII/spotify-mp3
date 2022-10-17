dfefb6e0fa9c0ac296a0dcca549c259634a57f0a
Loïs L <36632834+LifestreamVII@users.noreply.github.com>
2022-10-18 00:35:09 +0200

    fixing the listening address/port

--- scripts/app.py
+++ scripts/app.py
@@ -117,7 +117,7 @@ def taskstatus(task_id):
 
 if __name__ == "__main__":
 	# for debugging locally
-	app.run(debug=True, host='10.0.1.3',port=5000)
+	app.run(debug=True, host='0.0.0.0',port=5000)
 	
 	# for production
 	#app.run(host='0.0.0.0', port=5000)
